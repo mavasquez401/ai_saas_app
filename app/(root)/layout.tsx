3ec672e0fae3865814259d5d9464d2f4ba81834b
mavasquez401 <mavasquez401@gmail.com>
2024-07-13 18:55:36 -0400

    empty

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,14 +3,17 @@ import SideBarMain from '@/components/shared/SideBarMain';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="root">
-      <SideBarMain />
-      <MobileNavi />
-      <div className="root-container">
-        <div className="wrapper">{children}</div>
-      </div>
-    </main>
+    <>
+      <main className="root">
+        <SideBarMain />
+        <MobileNavi />
+        <div className="root-container">
+          <div className="wrapper">{children}</div>
+        </div>
+      </main>
+    </>
   );
 };
 
 export default Layout;
+// layout setup
