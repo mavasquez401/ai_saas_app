d0ca08ee11fecc0db0773d0a36759b8e8e5dcb26
mavasquez401 <mavasquez401@gmail.com>
2024-08-02 23:14:12 -0400

    have to fix mongo

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,4 +1,33 @@
 import mongoose, { Mongoose } from 'mongoose';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = process.env.MONGODB_URL2;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run(): Promise<void> {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
@@ -32,33 +61,3 @@ export const connectToDatabase = async () => {
 
   return cached.conn;
 };
-
-import { MongoClient, ServerApiVersion } from 'mongodb';
-
-const uri = process.env.MONGODB_URL2;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run(): Promise<void> {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
